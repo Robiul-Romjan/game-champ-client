f4011f59fb0de16afae862599a985f7e5c4eb36f
robiul-romjan <robiulromjan7@gmail.com>
2023-06-08 01:07:12 +0600

    admin work done

--- src/pages/DashboardPage/ManageUsers/ManageUsers.jsx
+++ src/pages/DashboardPage/ManageUsers/ManageUsers.jsx
@@ -1,10 +1,36 @@
 import { useQuery } from "@tanstack/react-query"
+import { useState } from "react";
 
 const ManageUsers = () => {
+
     const { data: users = [], refetch } = useQuery(["users"], async () => {
         const res = await fetch("http://localhost:5000/users")
         return res.json();
     });
+
+    const handleMakeAdmin = (id) => {
+        fetch(`http://localhost:5000/users/admin/${id}`, {
+            method: "PATCH",
+        })
+        .then(res => res.json())
+        .then(data => {
+            if(data.modifiedCount> 0){
+                alert("user now admin")
+            }
+        })
+    };
+
+    const handleMakeInstructor = (id) => {
+        fetch(`http://localhost:5000/users/instructor/${id}`, {
+            method: "PATCH",
+        })
+        .then(res => res.json())
+        .then(data => {
+            if(data.modifiedCount> 0){
+                alert("user now Instructor")
+            }
+        })
+    };
 
     return (
         <div>
@@ -23,14 +49,18 @@ const ManageUsers = () => {
                     </thead>
                     <tbody>
                         {
-                            users.map((user, i)=> <tr key={user._id}>
-                            <th>{i+1}</th>
-                            <td>{user.name}</td>
-                            <td>{user.email}</td>
-                            <td><button className="btn btn-sm btn-info">Make Instructor</button></td>
-                            <td><button className="btn btn-sm btn-error">Make Admin</button></td>
-                        </tr>)
-                        }              
+                            users.map((user, i) => <tr key={user._id}>
+                                <th>{i + 1}</th>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td>
+                                    <button onClick={()=> handleMakeInstructor(user._id)} className="btn btn-sm btn-info" disabled={user.role == "instructor"}>Make Instructor</button>
+                                </td>
+                                <td>
+                                    <button onClick={()=> handleMakeAdmin(user._id)} className="btn btn-sm btn-error" disabled={user.role == "admin"} >Make Admin</button>
+                                </td>
+                            </tr>)
+                        }
                     </tbody>
                 </table>
             </div>
